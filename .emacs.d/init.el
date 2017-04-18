ff4e97cd47505e77fb88b7a934564adcca14385e
Hidekazu Kasagi <kasagi@cocolable.co.jp>
2017-04-18 15:54:45 +0900

    emacs25.1.1にアップデートしたら、Packate.elが有効になったので向こうにする

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,13 @@
 ;; 隔離された環境にパッケージをインストール
 ; (例：パッケージを指定して起動)　emacs -q -l ~/path/to/somewhere/init.el
 ;
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
