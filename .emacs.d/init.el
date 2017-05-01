289d6f944723457113fcf2fde06943b99ad8a0a7
Hidekazu Kasagi <kasagi@cocolable.co.jp>
2017-05-01 10:14:25 +0900

    エラーになるため、設定を削除

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -137,7 +137,6 @@
 (el-get-bundle mooz/js2-mode)
 ; jsxファイルに適用
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
-(flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
 (add-hook 'js2-jsx-mode-hook 'flycheck-mode)
 
 
