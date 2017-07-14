c076ff0dbb833e70cdc7b7568838bf05d79dc91e
Hidekazu Kasagi <kasagi@cocolable.co.jp>
2017-07-14 19:26:44 +0900

    gs拡張子のgasファイルをjs2modeで開く、js2モードのインデントを2に変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -137,8 +137,9 @@
 (el-get-bundle mooz/js2-mode)
 ; jsxファイルに適用
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.gs\\'" . js2-mode))
 (add-hook 'js2-jsx-mode-hook 'flycheck-mode)
-
+(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
 
 ;;;
 ;;; howm 設定
